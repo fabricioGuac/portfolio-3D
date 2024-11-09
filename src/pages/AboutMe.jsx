9539cacd8ea0305406ae6eac2c4e3208eaba21f2
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-11-08 20:24:21 -0800

    delves in the about me page

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -5,7 +5,24 @@ import Header from "../components/Header";
 export default function AboutMe () {
     return (<>
     <Header title={"About me"}/>
-    <h1>About me</h1>
+    <div className="beach">
+        <div className="curved-trunk"></div>
+        <div className="coconut coconut-1"></div>
+        <div className="coconut coconut-2"></div>
+        <div className="coconut coconut-3"></div>
+        <h3 className="about-me-text">
+        Hello! I'm Fabricio Guacuto,
+        an aspiring Full Stack Developer
+        with a strong foundation in the MERN stack 
+        and a keen focus on backend development.
+        Having completed a rigorous bootcamp,
+        I've built a solid skill set
+        through hands-on projects and collaborative learning.
+        Known for my dedication, attention to detail, and work ethic,
+        I'm eager to contribute to a dynamic team where my passion for problem-solving
+        and backend expertise can make a difference.
+        </h3>
+    </div>
     <Footer/>
     </>)
 };
